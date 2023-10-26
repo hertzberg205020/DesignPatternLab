64e1e18d3bff087da34322f9eff13164f08212dc
N000187180 <robert5519@fpg.com.tw>
2023-10-26 17:18:25 +0800

    fix: 修正ShowDowmGame中的OnGameEnded，數位玩家分數相同且為冠軍的情況

--- CardGameFramework/Models/ShowDownGame/ShowDownGame.cs
+++ CardGameFramework/Models/ShowDownGame/ShowDownGame.cs
@@ -66,7 +66,7 @@ public class ShowDownGame : CardGame<PokerCard>
         ShowWinners(winners);
     }
 
-    private static void ShowWinners(List<IShowDownGamePlayer> winners)
+    private void ShowWinners(List<IShowDownGamePlayer> winners)
     {
         if (winners.Count == 1)
         {
