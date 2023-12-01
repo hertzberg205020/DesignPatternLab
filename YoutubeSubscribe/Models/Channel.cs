beb3bb2eb70820ef325a3eae9e8c6293d17a142a
N000187180 <robert5519@fpg.com.tw>
2023-12-01 10:44:50 +0800

    feat: fix the SDK version

--- YoutubeSubscribe/Models/Channel.cs
+++ YoutubeSubscribe/Models/Channel.cs
@@ -3,21 +3,25 @@
 public class Channel
 {
     public string? Name { get; set; }
-    
+
     private readonly List<ChannelSubscriber> _subscribers = new();
-    
+
     private readonly List<Video> _videos = new();
 
     public Channel(string name)
     {
         Name = name;
     }
-    
+
     public void Subscribe(ChannelSubscriber channelSubscriber)
     {
         _subscribers.Add(channelSubscriber);
     }
 
+    /// <summary>
+    /// Unsubscribes a channel subscriber from the channel.
+    /// </summary>
+    /// <param name="channelSubscriber">The channel subscriber to unsubscribe.</param>
     public void UnSubscribe(ChannelSubscriber channelSubscriber)
     {
         var target = _subscribers.Find(c => c.Name == channelSubscriber.Name);
@@ -27,11 +31,11 @@ public class Channel
             _subscribers.Remove(target);
         }
     }
-    
+
     /// <summary>
-    /// upload video to channel
+    /// Uploads a video to the channel.
     /// </summary>
-    /// <param name="video"></param>
+    /// <param name="video">The video to be uploaded.</param>
     public void UploadVideo(Video video)
     {
         Console.WriteLine($"頻道 {Name} 上傳影片 {video.Title}");
@@ -47,7 +51,7 @@ public class Channel
     public void NotifyObservers(Video video)
     {
         var subscribersSnapshot = new List<ChannelSubscriber>(_subscribers);
-        
+
         foreach (var subscriber in subscribersSnapshot)
         {
             subscriber.ReceivedNotification(video);
