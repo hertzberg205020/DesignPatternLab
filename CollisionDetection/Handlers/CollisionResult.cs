c56b05529006066bd529b345c93ce7b6d7911b52
Kang <u8360182@gmail.com>
2024-01-04 22:53:44 +0800

    feat(CollisionDetection): add CollisionReust

---
+++ CollisionDetection/Handlers/CollisionResult.cs
@@ -0,0 +1,18 @@
+﻿using CollisionDetection.Models;
+
+namespace CollisionDetection.Handlers;
+
+/// <summary>
+/// 不建立 Sprite 與 World 的雙向關聯就需要碰撞結果來告知 World 要不要移除 Sprite
+/// </summary>
+public class CollisionResult
+{
+    public readonly bool IsSuccess;
+
+    public Sprite? RemovedSprite { get; set; }
+    
+    public CollisionResult(bool isSuccess)
+    {
+        IsSuccess = isSuccess;
+    }
+}
