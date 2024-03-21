3dee7986e27e8f69c30490c0d2b5bbfed00ca6c1
N000187180 <robert5519@fpg.com.tw>
2024-03-21 12:18:30 +0800

    feat(Receiver): Add two receiver

---
+++ ShortCutSettings/Receivers/Telecom.cs
@@ -0,0 +1,14 @@
+﻿namespace ShortCutSettings.Receiver;
+
+public class Telecom
+{
+    public void Connect()
+    {
+        Console.WriteLine("The telecom has been turned on.");
+    }
+    
+    public void Disconnect()
+    {
+        Console.WriteLine("The telecom has been turned off.");
+    }
+}
